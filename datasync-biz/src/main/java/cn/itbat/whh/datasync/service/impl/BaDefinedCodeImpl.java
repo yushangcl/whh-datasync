e0ca9fab14d6b98b3754b190ceaafb83e9264e5d
log.r <log.r@live.cn>
2018-04-15 21:56:47 +0800

    datasync init

--- datasync-biz/src/main/java/cn/itbat/whh/datasync/service/impl/BaDefinedCodeImpl.java
+++ datasync-biz/src/main/java/cn/itbat/whh/datasync/service/impl/BaDefinedCodeImpl.java
@@ -1,10 +1,12 @@
 package cn.itbat.whh.datasync.service.impl;
 
 import cn.itbat.whh.datasync.service.BaDefinedCodeService;
+import org.springframework.stereotype.Service;
 
 /**
  * @author log.r   (;￢＿￢)   
  * @date 2018-04-15 下午9:43
  **/
+@Service
 public class BaDefinedCodeImpl implements BaDefinedCodeService {
 }
